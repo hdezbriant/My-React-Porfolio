b38dd178ac2b5e55fba524dd13f99534d7b1d323
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-18 23:49:27 -0700

    changed waiting logo and updated message

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://64.media.tumblr.com/23a38114e102323b5a475324e2e4972c/tumblr_inline_pg39xyDjM01u2s4s8_500.gifv" alt="logo" />
+        Hey. I'm working on it! <br></br>Please, come back later. <br></br><br></br>
+        Hang out with my bats in the meantime.
       </header>
     </div>
   );
