04196b30113e4adfcd6c3183f9ffe8145cd6c80f
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-28 05:39:12 -0700

    adding header component, removed bats :(

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://64.media.tumblr.com/23a38114e102323b5a475324e2e4972c/tumblr_inline_pg39xyDjM01u2s4s8_500.gifv" alt="logo" />
-        Hey. I'm working on it! <br></br>Please, come back later. <br></br><br></br>
-        Hang out with my bats in the meantime.
-      </header>
+      <Header />
+      Hey. I'm working on it! <br></br>Please, come back later.
     </div>
   );
 }
