ece94e3e928131e6f75e942a7f59987b72576eb2
Entropy <79678575+hdezbriant@users.noreply.github.com>
2021-05-28 07:10:46 -0700

    completed footer component with links

---
+++ src/components/footer/footer.js
@@ -0,0 +1,24 @@
+import { Navbar, Nav } from "react-bootstrap";
+
+function Footer() {
+  return (
+    <Navbar bg="dark" variant="dark" id="connect">
+      <Nav className="mx-auto">
+        <Nav.Link href="https://github.com/hdezbriant" className="text-warning">
+          Github
+        </Nav.Link>
+        <Nav.Link
+          href="https://www.linkedin.com/in/brian-hernandez-309b2a20a/"
+          className="text-warning"
+        >
+          Linkedin
+        </Nav.Link>
+        <Nav.Link mailto="hernandez.briant@gmail.com" className="text-warning">
+          E-mail me!
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+}
+
+export default Footer;
